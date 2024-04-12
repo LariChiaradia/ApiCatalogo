e6d6e704bfab43b70ba386365f7b0ce36a1ade77
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-04-12 09:14:44 -0300

    First Commit

---
+++ ApiCatalogo/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Context
+{
+    //É responsável por realizar a comunicação entre as entidades e o banco de dados relacional (Mapeamento)
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
