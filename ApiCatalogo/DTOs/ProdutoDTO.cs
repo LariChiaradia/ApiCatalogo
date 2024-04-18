2bd08a202b100c12f4cef5d547b021ba8bab2f5d
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-04-18 16:02:39 -0300

    feat/Criada ProdutoDTO

--- ApiCatalogo/DTOs/ProdutoDTO.cs
+++ ApiCatalogo/DTOs/ProdutoDTO.cs
@@ -1,11 +1,21 @@
-﻿namespace ApiCatalogo.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCatalogo.DTOs
 {
     public class ProdutoDTO
     {
         public int ProdutoId { get; set; }
+        [Required]
+        [StringLength(80)]
         public string Nome { get; set; }
+        [Required]
+        [StringLength(300)]
         public string Descricao { get; set; }
+        [Required]
         public decimal preco { get; set; }
+        [Required]
+        [StringLength(300)]
         public string ImageUrl { get; set; }
         public int CategoriaId { get; set; }
     }
